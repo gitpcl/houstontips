a999e76eb79057435f41e1df6ac0b957323254f0
Pedro Lopes <pedrolopes@Pedros-MacBook-Pro.local>
2020-10-11 16:09:21 -0500

    Updated navbar

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,7 @@
-const mainMenu = document.querySelector('.mainMenu')
-const closeMenu = document.querySelector('.closeMenu')
-const openMenu = document.querySelector('.openMenu')
-
-openMenu.addEventListener('click', show)
-closeMenu.addEventListener('click', close)
-
-function show(){
-    mainMenu.style.display = 'flex'
-    mainMenu.style.top = '0'
+function toggleNavOpen() {
+    document.querySelector(".navbar").classList.toggle("navbar-open");
 }
 
-function close(){
-    mainMenu.style.top = '-100%'
+function toggleNavClose() {
+    document.querySelector(".navbar").classList.remove("navbar-open");
 }
