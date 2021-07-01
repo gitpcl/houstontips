163c650f03c4c49ee4e469b7941531b6626ee5f9
Pedro <admin@pedroclopes.com>
2021-07-01 18:22:55 -0500

    Added a long lived access token

--- scripts/count.js
+++ scripts/count.js
@@ -1,5 +1,5 @@
 // JQuery Solution
-$.getJSON("https://graph.facebook.com/v10.0/17841403134443963?fields=business_discovery.username(houstontips)%7Bfollowers_count%2Cmedia_count%7D&access_token=EAALMiACnWLABAIuYvvm0GQIkhpV1ZBxfv8UlRrTLffnfl2IpGuRHYLvColXbIZAWsrCJfZBLBHKf4IFtL3WIEEw6t5qfT9ZBZBFhXRIDyiXZB4pDi5wtvoBQz0Ihy8foYXMTpbmMezCILVnrJbibu0CUjbIhsJMpZC3mWzkTMIPqoJ4cmXNBLr4ZABp9DesUBhXdl1OXIxdqsBPSJSoJeFsf", function(data) {
+$.getJSON("https://graph.facebook.com/v11.0/17841403134443963?fields=business_discovery.username(houstontips)%7Bfollowers_count%2Cmedia_count%7D&access_token=EAALMiACnWLABAKJblK5u7EVzmAJB8HVPZBvhwDbUlRWVMz9XVz1hlZBi8ctD4hBKHCGvATUEIkhp2nxCG8CDXj9eVjubWt0TvM9jlNSfX2e3u5O46cPTOqD9SjkdLMbwRRUkpU3pRa0Yg7OPQevRFhWxIzxuTRhyZBTpBZATqwZDZD", function(data) {
 
     const followers = data.business_discovery.followers_count/1000;
     const media = data.business_discovery.media_count;
@@ -27,3 +27,15 @@ $.getJSON("https://graph.facebook.com/v10.0/17841403134443963?fields=business_di
 //       console.log(response);
 //   }
 // );
+
+
+// let requestURL = 'https://graph.facebook.com/v11.0/17841403134443963?fields=business_discovery.username(houstontips)%7Bfollowers_count%2Cmedia_count%7D&access_token=EAALMiACnWLABAEQA9db4ve5gV5pkEPboxnnZAOivv2YZBm5y4PepGmZCWcyOzrZBPEeOrp2yOAc6ZAZA77rVjp9C0dlkRI3g3WZA4Pu9e5tRObwtDsna3yAytD0hCc2vR2JzIAVyEsTLjAwbjGl3pdgPUZAeHDoUTCmRkOPk2mH4cSua3evgfqVPosok5OwIwullycpI5wGaUZA83Rtj7rtD7';
+// let request = new XMLHttpRequest();
+// request.open('GET', requestURL);
+// request.responseType = 'json';
+// request.send();
+
+// // Begin accessing JSON data here
+// var data = JSON.parse(this.response)
+
+// console.log(data)
