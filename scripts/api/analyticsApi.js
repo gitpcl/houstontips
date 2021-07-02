b707e62924de7dc780046a86090e4c1e5cd0612e
Pedro <admin@pedroclopes.com>
2021-07-02 00:22:15 -0500

    Updated API javascript and hide the token

--- scripts/api/analyticsApi.js
+++ scripts/api/analyticsApi.js
@@ -21,27 +21,36 @@
 
 
 // Javascript Solution
-fetch('https://graph.facebook.com/v11.0/17841403134443963?fields=business_discovery.username(houstontips)%7Bfollowers_count%2Cmedia_count%7D&access_token=EAALMiACnWLABAGXFhwlSBY464JGIiN14g6q7JB9SAB6RflieSfaajtRmIZASN6bUZAL0IFFaf5O5owFZA8ri2Gtp1ZC3sW1paHZCKQHFHzpDCHr2jI1kgFwCTZBZAzoAR7rNPzTzDZBll3UWMIsBYP0sLpES6W6jxGTDL4z8NVCUSHXZBWN44ZBnvu')
-  .then(response => response.json())
-  .then(data => {
+const base_url = 'https://graph.facebook.com/v11.0/';
+const page_id = '17841403134443963';
+const access_token = config.ACCESS_TOKEN;
 
-    const followers = data.business_discovery.followers_count/1000;
-    const media = data.business_discovery.media_count;
-      
-    function commaSeparateNumber(val){
-        while (/(\d+)(\d{3})/.test(val.toString())){
-          val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
-        }
-        return val;
+const url = base_url + page_id + '?fields=followers_count,media_count,media&access_token=' + access_token;
+console.log(url);
+
+fetch(url)
+  .then(response => response.json())
+  .then(data =>
+    // console.log(data)
+    {
+      const followers = data.followers_count/1000;
+      const media = data.media_count;
+        
+      function commaSeparateNumber(val){
+          while (/(\d+)(\d{3})/.test(val.toString())){
+            val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
+          }
+          return val;
+      }
+
+      const analyticsText = (followers).toFixed(1) + 'K' + '<p>Followers</p>';
+      const mediaText = commaSeparateNumber(media + '<p>Media Posted</p>');
+
+      const analyticsEl = document.getElementsByClassName('followers_count')[0];
+      const mediaEl = document.getElementsByClassName('media_count')[0];
+
+      analyticsEl.innerHTML = analyticsText;
+      mediaEl.innerHTML = mediaText;
     }
 
-    const analyticsText = (followers).toFixed(1) + 'K' + '<p>Followers</p>';
-    const mediaText = commaSeparateNumber(media + '<p>Media Posted</p>');
-
-    const analyticsEl = document.getElementsByClassName('followers_count')[0];
-    const mediaEl = document.getElementsByClassName('media_count')[0];
-
-    analyticsEl.innerHTML = analyticsText;
-    mediaEl.innerHTML = mediaText;
-
-  });
+  );
--- scripts/api/landingApi.js
+++ scripts/api/landingApi.js
@@ -1,11 +1,20 @@
-fetch('https://graph.facebook.com/v11.0/17841403134443963?fields=business_discovery.username(houstontips)%7Bfollowers_count%2Cmedia_count%7D&access_token=EAALMiACnWLABAGXFhwlSBY464JGIiN14g6q7JB9SAB6RflieSfaajtRmIZASN6bUZAL0IFFaf5O5owFZA8ri2Gtp1ZC3sW1paHZCKQHFHzpDCHr2jI1kgFwCTZBZAzoAR7rNPzTzDZBll3UWMIsBYP0sLpES6W6jxGTDL4z8NVCUSHXZBWN44ZBnvu')
+const base_url = 'https://graph.facebook.com/v11.0/';
+const page_id = '17841403134443963';
+const access_token = config.ACCESS_TOKEN;
+
+const url = base_url + page_id + '?fields=followers_count,media_count,media&access_token=' + access_token;
+console.log(url);
+
+fetch(url)
   .then(response => response.json())
-  .then(data => {
+  .then(data =>
+    // console.log(data)
+    {
+      const followers = data.followers_count/1000;
 
-    const followers = data.business_discovery.followers_count/1000;
-      
-    const landingText = (followers).toFixed(1) + ' K';
-    const landingEl = document.getElementsByClassName('followers_count_landing')[0];
-    landingEl.innerHTML = landingText;
+      const landingText = (followers).toFixed(1) + ' K';
+      const landingEl = document.getElementsByClassName('followers_count_landing')[0];
+      landingEl.innerHTML = landingText;
+    }
 
-  });
+  );
