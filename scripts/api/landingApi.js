df84022faf4c7099b814003c4001809ba01c20fc
Pedro <admin@pedroclopes.com>
2021-07-07 23:55:22 -0500

    Updated API variable name

--- scripts/api/landingApi.js
+++ scripts/api/landingApi.js
@@ -1,6 +1,6 @@
 const base_url = 'https://graph.facebook.com/v11.0/';
 const page_id = '17841403134443963';
-const access_token = config.ACCESS_TOKEN;
+const access_token = config.FB_ACCESS_TOKEN ;
 
 const url = base_url + page_id + '?fields=followers_count,media_count,media&access_token=' + access_token;
 console.log(url);
